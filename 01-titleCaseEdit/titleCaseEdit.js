9fb88e11c167cb0d0e64c92b3ecc7cb4078e5443
dkamalov <dkamalov8@gmail.com>
2020-12-25 15:52:49 -0500

    first1

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,19 @@
 function titleCaseEdit(title) {
-  // Insert code here;
-}
 
+  //title.toLowerCase().split(" ");
+
+  let s = " ";
+
+  String(title).charAt(0).toUpperCase();
+
+  for(let i = 0; i < String(title).length; i++){
+
+    if(String(title).charAt(i) === s){
+      String(title).charAt(i+1).toUpperCase();
+    }
+
+  }
+  return String(title);
+}
 // Do not edit this line;
 module.exports = titleCaseEdit;
