6d4e0ff33373088eaa45c6221395ba0c83645a5f
dkamalov <dkamalov8@gmail.com>
2020-12-28 08:54:54 -0500

    Update sumOfAllOddNumbers.js

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,12 +1,14 @@
 function sumOfAllOddNumbers(nums) {
+
   let count = 0;
       
   for (let i = 0; i <= nums.length; i++){
 
-    if (nums[i]%2 !== 0){
+    if (nums[i]%2 === 1){
       count++;
     }
   }
+
  return count;
 }
 
