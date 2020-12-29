2ba2ded587de2a9efa6195bf11e21c238fefd66f
dkamalov <dkamalov8@gmail.com>
2020-12-29 09:40:50 -0500

    good morning

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,20 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+ 
+let min = nums[0];
+let max = nums[0];
+
+  for (let i = 0; i <= nums.length; i++){
+    
+    if (nums[i] >= max){
+      max = nums[i];
+    }
+
+    if (nums[i] <= min) {
+      min = nums[i];
+    }
+  }
+
+  return max+min;
 }
 
 // Do not edit this line;
