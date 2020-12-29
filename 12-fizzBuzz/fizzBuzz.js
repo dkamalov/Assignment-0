2c2be91172060eb0daf0f82a19000ac85f1bc30e
dkamalov <dkamalov8@gmail.com>
2020-12-29 10:41:36 -0500

    Long one

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,30 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  
+  let outArray = [];
+
+  for (let i = start; i <= end; i++){
+    
+    if(i % 3 === 0 && i % 5 === 0){
+      outArray.push("FizzBuzz");
+  
+    }
+
+    else if(i % 3 === 0){
+      outArray.push("Fizz");
+     
+    }
+
+    else if(i % 5 === 0){
+      outArray.push("Buzz");
+      
+    } 
+    
+    else{
+      outArray.push(i);
+    }
+  }
+
+  return outArray;
 }
 
 // Do not edit this line;
