35db911cce800a930307e81e0f586556cdfb98a2
dkamalov <dkamalov8@gmail.com>
2020-12-30 09:33:55 -0500

    Very good start to the day

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,21 @@
 function pairSum(nums, target) {
-  // Insert code here;
+
+  if(nums.length <= 1){
+    throw Error;
+  }
+  
+    for (let i = 0; i < nums.length; i++) {
+
+      for (let j = i + 1; j < nums.length; j++){
+
+        if (nums[i] + nums[j] === target){
+
+          return true;
+        }
+      }
+    }
+
+    return false;
 }
 
 // Do not edit this line;
