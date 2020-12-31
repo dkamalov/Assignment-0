c363ce85cb5474c2f3e1040621ad4b271aa12b30
dkamalov <dkamalov8@gmail.com>
2020-12-30 22:36:27 -0500

    The Finale
    Hardest one personally. 
    Whole file system on computer was corrupted and now I need to fix everything tomorrow. God I hate Onedrive, it almost ruined my entire computer, I had to  redo the entire environment on visual studio because of Onedrive. I turned it off and good things this happened now before the course. Wow I just hate Onedrive so much.

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,23 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  
+  let wordArray = word.split("");
+  let frequency = {};
+
+  for (let i = 0; i <= word.length; i++){
+
+    let l = wordArray[i];
+    
+    if (wordArray.hasOwnProperty(i)){
+
+      if(!frequency[l]){
+        frequency[l] = 0;
+      } 
+
+      frequency[l]++;
+    }
+  }
+
+  return frequency;
 }
 
 // Do not edit this line;
