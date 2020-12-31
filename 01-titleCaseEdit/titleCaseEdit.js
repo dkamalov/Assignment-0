a00c174aa898e6f3d3c41ff20e2b4d873a705ec6
dkamalov <dkamalov8@gmail.com>
2020-12-30 22:39:17 -0500

    little change

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,15 +1,10 @@
 function titleCaseEdit(title) {
 
-//Split into array 
   let t = title.split(' ');
 
   for (let i = 0; i < t.length; i++) {
-
-//Capitilize the first one
       t[i] = t[i].charAt(0).toUpperCase() + t[i].substring(1);     
   }
-
-//Put it back into a string
   return t.join(' '); 
 }
 // Do not edit this line;
